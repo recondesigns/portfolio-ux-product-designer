fa2d97d76a0f6a6df8087c7f86c39982e27acb1e
Michael Stedman <46408716+recondesigns@users.noreply.github.com>
2021-04-14 22:02:49 -0500

    Commit

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,7 +2,6 @@ import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import { AppContext } from '../../context/AppState'
-// import hamburgerClosedIcon from '../../assets/icons/hamburger-closed.svg'
 import { black, white, green } from '../../colors/'
 
 const NavigationContainer = styled.div`
@@ -23,99 +22,8 @@ const NavigationContainer = styled.div`
         width: 236px;
     }
 
-    // & > img {
-    //     position: absolute;
-    //     top: 20px;
-    //     left: 24px;    
-    //     border: 1px solid orange;
-    // }
-    
-    // & > .label {
-    //     margin: 0px;
-    //     width: 100%;
-    //     font-family: Roboto;
-    //     font-style: normal;
-    //     font-weight: 500;
-    //     font-size: 18px;
-    //     line-height: 24px;
-    //     color: ${black.default};
-    //     border: 1px dotted orange;
-    // }
-
-    // & > .home-link {
-    //     color: red;
-    // }
-
-    // & > .label-ux {
-    //     position: absolute;
-    //     height: 24px;
-    //     left: 24px;
-    //     right: 24px;
-    //     top: 68px;
-    // }
-
-    // & > .label-dev {
-    //     position: absolute;
-    //     height: 24px;
-    //     left: 24px;
-    //     right: 24px;
-    //     top: 188px;
-    // }
-
-    // & > a {
-    //     font-family: Roboto;
-    //     font-style: normal;
-    //     font-weight: 400;
-    //     font-size: 16px;
-    //     line-height: 24px;
-    //     display: flex;
-    //     align-items: center;
-    //     color: ${black.default};
-    //     text-decoration: none;
-    //     border: 1px dotted orange;
-
-    //     &:hover {
-    //         font-weight: 500;
-    //         text-decoration: underline;
-    //     }
-    // }
-
-    // & > .ux-project-one {
-    //     position: absolute;
-    //     height: 24px;
-    //     left: 40px;
-    //     right: 24px;
-    //     top: 96px;
-    // }
-
-    // & > .ux-project-two {
-    //     position: absolute;
-    //     height: 24px;
-    //     left: 40px;
-    //     right: 24px;
-    //     top: 124px;
-    // }
-
-    // & > .ux-project-three {
-    //     position: absolute;
-    //     height: 24px;
-    //     left: 40px;
-    //     right: 24px;
-    //     top: 152px;
-    // }
-
     & > .home-link {
         margin-bottom: 4px;
-        // width: 100%;
-        // font-family: Roboto;
-        // font-style: normal;
-        // font-weight: 500;
-        // font-size: 18px;
-        // line-height: 24px;
-        // display: flex;
-        // align-items: center;
-
-        // color: ${black.default};
         // border: 1px dotted red;
 
         &:hover {
@@ -126,16 +34,6 @@ const NavigationContainer = styled.div`
 
     & > .list-item {
         margin-bottom: 4px;
-        // width: 100%;
-        // font-family: Roboto;
-        // font-style: normal;
-        // font-weight: 500;
-        // font-size: 18px;
-        // line-height: 24px;
-        // display: flex;
-        // align-items: center;
-
-        // color: ${black.default};
         // border: 1px dotted blue;
     }
 
@@ -147,10 +45,6 @@ const NavigationContainer = styled.div`
         font-weight: 400;
         font-size: 16px;
         line-height: 24px;
-        // display: flex;
-        // align-items: center;
-        color: ${black.default};
-        text-decoration: none;
         // border: 2px solid green;
 
         &:hover {
@@ -179,10 +73,8 @@ export default function Navigation() {
 
     function openCloseMenu() {
         if (isMenuOpen === false) {
-            // console.log('it is false and not open')
             return 'none'
         } else if (isMenuOpen === true) {
-            // console.log('it is true and is open')
             return 'block'
         }
     }
@@ -197,8 +89,6 @@ export default function Navigation() {
 
     return (
         <NavigationContainer display={navPosition}>
-            {/* <img src={hamburgerClosedIcon} alt={'close'} /> */}
-            {/* <p className={'label label-ux'}>{'UX Designer'}</p> */}
             <Item 
                 to={'/'}
                 className={'home-link'}
@@ -228,17 +118,6 @@ export default function Navigation() {
                 className={'list-sub-item'}
                 onClick={() => closeMenu()}
             >{'V School Map'}</Item>
-
-            {/* <Link to={'/'} className={'ux-project-one'} onClick={() => closeMenu()} >{'Home'}</Link> */}
-            {/* <Link to={'/project-one'} className={'ux-project-one'} onClick={() => closeMenu()} >{'Project One'}</Link> */}
-            {/* <Link to={'/project-two'} className={'ux-project-two'} onClick={() => closeMenu()} >{'Project Two'}</Link> */}
-            {/* <Link to={'/project-three'} className={'ux-project-three'} onClick={() => closeMenu()} >{'Project Three'}</Link> */}
-
-
-            {/* <p className={'label label-dev'}>{'Fullstack Dev'}</p> */}
-            {/* <p className={'sub-item dev-one'}>{'Project name'}</p> */}
-            {/* <p className={'sub-item dev-two'}>{'Project name'}</p> */}
-            {/* <p className={'sub-item dev-three'}>{'Project name'}</p> */}
         </NavigationContainer>
     )
 }
